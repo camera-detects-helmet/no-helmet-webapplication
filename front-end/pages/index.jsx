5ddbe704428a8fe7d74f4490d2a30cbfb9e3dca4
Piyawun <Piyawun.min@gmail.com>
2023-01-17 16:38:58 +0700

    edit pickdate
    add logo and pick date

--- front-end/pages/index.jsx
+++ front-end/pages/index.jsx
@@ -7,9 +7,18 @@ export default function Home() {
         <div className="rounded bg-white h-40 shadow-sm"></div>
         <div className="rounded bg-white h-40 shadow-sm"></div>
         <div className="rounded bg-white h-40 shadow-sm"></div>
+
+        <div className="relative">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-orange-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd" /></svg>
+        </div>
+        <input datepicker datepicker-buttons type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-orange-100 dark:border-orange-600 dark:placeholder-orange-500  dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date" />
+      </div>
+
       </div>
       <div className="grid col-1 bg-white mb-20 shadow-sm overflow-y-auto h-screen">
-
+    
         <div className=" bg-white-50">
           {/* Header */}
         
